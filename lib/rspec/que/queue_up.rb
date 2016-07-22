83e65d237f291c58f0b460c0aef5036ad54a5c50
Thomas Whaples <twhaples@gocardless.com>
2016-07-22 16:40:41 +0100

    queue_up().at(time) works

--- lib/rspec/que/queue_up.rb
+++ lib/rspec/que/queue_up.rb
@@ -1,9 +1,79 @@
 require 'rspec/mocks/argument_list_matcher'
+require 'time'
 
 module RSpec
   module Que
     module Matchers
       class QueueUp
+        def initialize(job_class = nil)
+          @matchers = [QueuedSomething.new]
+          @matchers << QueuedClass.new(job_class) if job_class
+          @job_class = job_class
+          @stages = []
+        end
+
+        def matches?(block)
+          before_jobs = enqueued_jobs.dup
+          block.call
+
+          @matched_jobs = enqueued_jobs - before_jobs
+          @matchers.each do |matcher|
+            @stages << {matcher: matcher, candidates: @matched_jobs.dup}
+            @matched_jobs.delete_if {|job| !matcher.matches?(job) }
+          end
+          @matched_jobs.any?
+        end
+
+        def with(*args)
+          @matchers << QueuedArgs.new(args)
+          self
+        end
+
+        def at(the_time)
+          matcher = QueuedAt.new(the_time)
+          @matchers << matcher
+          self
+        end
+
+        def failure_message
+          # last stage to have any candidate jobs
+          failed_stage = @stages.reject {|s| s[:candidates].empty? }.last || @stages.first
+          failed_matcher = failed_stage[:matcher]
+          failed_candidates = failed_stage[:candidates]
+          found_instead = failed_matcher.failed_msg(failed_candidates)
+
+          "expected to enqueue #{job_description}, but found #{found_instead}"
+        end
+
+        def failure_message_when_negated
+          "expected not to enqueue #{job_description}, got %d enqueued: %s" %
+            [@matched_jobs.length, @matched_jobs.map { |j| format_job(j) }.join(", ")]
+        end
+
+        def supports_block_expectations?
+          true
+        end
+
+        def description
+          return "queues up a #{job_description}"
+        end
+
+        private
+
+        attr_reader :before_count, :after_count, :job_class, :argument_list_matcher
+
+        def enqueued_jobs
+          ::Que.execute "SELECT * FROM que_jobs"
+        end
+
+        def job_description
+          @matchers.map(&:desc).join(" ")
+        end
+
+        def format_job(job)
+          "#{job[:job_class]}[" + job[:args].join(", ") + "]"
+        end
+
         class QueuedSomething
           def matches?(job)
             true
@@ -59,79 +129,22 @@ module RSpec
         end
 
         class QueuedAt
-          def initiaize(time)
-            @time = time
+          def initialize(the_time)
+            @time = the_time
           end
           def matches?(job)
-            # TODO implement and test
+            job[:run_at] == @time
           end
           def desc
+            "at #{@time}"
           end
-        end
-
-        def initialize(job_class = nil)
-          @matchers = [QueuedSomething.new]
-          @matchers << QueuedClass.new(job_class) if job_class
-          @job_class = job_class
-          @stages = []
-        end
-
-        def matches?(block)
-          before_jobs = enqueued_jobs.dup
-          block.call
-
-          @matched_jobs = enqueued_jobs - before_jobs
-          @matchers.each do |matcher|
-            @stages << {matcher: matcher, candidates: @matched_jobs.dup}
-            @matched_jobs.delete_if {|job| !matcher.matches?(job) }
+          def failed_msg(candidates)
+            if candidates.length == 1
+              "job at #{candidates.first[:run_at].to_s}"
+            else
+              "jobs at #{candidates.map {|c| c[:run_at]}}"
+            end
           end
-          @matched_jobs.any?
-        end
-
-        def with(*args)
-          raise "Must specify the job class when specifying arguments" unless job_class
-          @matchers << QueuedArgs.new(args)
-          self
-        end
-
-
-        def failure_message
-          # last stage to have any candidate jobs
-          failed_stage = @stages.reject {|s| s[:candidates].empty? }.last || @stages.first
-          failed_matcher = failed_stage[:matcher]
-          failed_candidates = failed_stage[:candidates]
-          found_instead = failed_matcher.failed_msg(failed_candidates)
-
-          "expected to enqueue #{job_description}, but found #{found_instead}"
-        end
-
-        def failure_message_when_negated
-          "expected not to enqueue #{job_description}, got %d enqueued: %s" %
-            [@matched_jobs.length, @matched_jobs.map { |j| format_job(j) }.join(", ")]
-        end
-
-        def supports_block_expectations?
-          true
-        end
-
-        def description
-          return "queues up a #{job_description}"
-        end
-
-        private
-
-        attr_reader :before_count, :after_count, :job_class, :argument_list_matcher
-
-        def enqueued_jobs
-          ::Que.execute "SELECT * FROM que_jobs"
-        end
-
-        def job_description
-          @matchers.map(&:desc).join(" ")
-        end
-
-        def format_job(job)
-          "#{job[:job_class]}[" + job[:args].join(", ") + "]"
         end
       end
     end
--- spec/rspec/que/queue_up_spec.rb
+++ spec/rspec/que/queue_up_spec.rb
@@ -117,4 +117,48 @@ RSpec.describe RSpec::Que::Matchers::QueueUp do
       end
     end
   end
+
+  context "with jobs queued at a specific time" do
+    let(:t1) { Time.parse("1997-10-03 01:23:45").utc }
+    let(:t2) { Time.parse("2000-03-21 06:07:08").utc }
+    let(:t3) { Time.parse("1991-07-07 13:57:11").utc }
+    let(:proc) do -> {
+        enqueued_jobs << { job_class: "AJob", args: ['madoka'], run_at: t1 }
+        enqueued_jobs << { job_class: "AJob", args: ['yuna'], run_at: t2 }
+        enqueued_jobs << { job_class: "BJob", args: ['senjougahara'], run_at: t3 }
+      }
+    end
+
+    describe '#at' do
+      it 'should match a job present at the specified time' do
+        expect(instance.at(t2).matches?(proc)).to eq(true)
+        expect(instance.failure_message_when_negated).
+          to eq("expected not to enqueue a job at 2000-03-21 06:07:08 UTC, got 1 enqueued: AJob[yuna]")
+      end
+      it 'should not match if no jobs are present' do
+        expect(instance.at(Time.now).matches?(proc)).to eq(false)
+      end
+      describe 'chaining with previous specifiers' do
+        it 'can chain with args' do
+          expect(instance.with('yuna').at(t3).matches?(proc)).to eq(false)
+          expect(instance.failure_message).
+            to eq(%(expected to enqueue a job with args ["yuna"] at #{t3}, but found job at #{t2}))
+        end
+        describe 'chaining with a class' do
+          let(:job_class) { AJob }
+          it "matches within the class" do
+            expect(instance.at(t3).matches?(proc)).to eq(false)
+            expect(instance.failure_message).
+              to eq(%(expected to enqueue a job of class AJob at #{t3}, but found jobs at [#{t1}, #{t2}]))
+          end
+
+        end
+        it 'can negative-chain' do
+          expect(instance.with('yuna').at(t2).matches?(proc)).to eq(true)
+          expect(instance.failure_message_when_negated).
+            to eq(%(expected not to enqueue a job with args ["yuna"] at #{t2}, got 1 enqueued: AJob[yuna]))
+        end
+      end
+    end
+  end
 end
